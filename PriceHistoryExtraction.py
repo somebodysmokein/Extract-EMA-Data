d8210ddc9de55723c46c335fc5eb8176aab7752b
somebodysmokein <venky88r@gmail.com>
2022-12-18 01:06:35 +0530

    Updated with the logic to pull all stocks that has closing price greater than 20 EMA

--- PriceHistoryExtraction.py
+++ PriceHistoryExtraction.py
@@ -1,55 +1,68 @@
 from nsepy import get_history
 from datetime import date, timedelta
 import pandas as pd
-def getPrice( symbol, today,  numdays):
-    startdt=today- timedelta(days=numdays*2)
+import math
+
+
+def getPrice(symbol, today, numdays):
+    startdt = today - timedelta(days=numdays * 2)
+    data = pd.DataFrame()
     data = get_history(symbol=symbol, start=startdt, end=today)
-    print("Price History of " + symbol + " from " + str(data.tail(numdays).index[0]) + " to expiry date: " + str(today))
-    print (data.tail(numdays))
-    #print(data.columns)
-    #print(data["Close"])
-    return data.tail(numdays)
+    if (data.empty):
+        print('Empty Price history')
+        return data
+    else:
+        print("Price History of " + symbol + " from " + str(data.tail(numdays).index[0]) + " to expiry date: " + str(
+            today))
+        print(data.tail(numdays))
+        # print(data.columns)
+        # print(data["Close"])
+        return data.tail(numdays)
 
 
 def extractcloseData(df):
     print("Closing data for last 20 days ")
-    print (df['Close'])
+    print(df['Close'])
+
 
 def calculateMVA(df):
-    #print(df.columns)
-    mva=df["Close"].mean()
-    #print("Simple Moving average: "+str(mva))
+    # print(df.columns)
+    mva = df["Close"].mean()
+    # print("Simple Moving average: "+str(mva))
     return mva
 
-def calculateEMA(symbol,df,numdays):
-    lastRow=df.tail(1)
-    #print(lastRow)
-    closingPriceToday=lastRow["Close"]
-    #print("closingPriceToday : "+str(closingPriceToday))
-    #print("Index"+ str(df.index[0]))
-    priordf = getPrice(symbol=symbol, today=df.index[0]-timedelta(days=1)
-                                          , numdays=numdays)
-    #extractcloseData(priordf)
-    #print(priordf)
-    #print(priordf.count)
-    prevEMA=calculateMVA(priordf)
-    #print("First EMA => "+str(prevEMA))
-    finalEMA=0
-    for ind in df.index:
-        currentEMA=emaforDate(prevEMA,df['Close'][ind],numdays)
-        #print("EMA for "+str(ind)+"  "+str(currentEMA))
-        #cummulativeEMA=prevEMA+currentEMA
-        prevEMA=currentEMA
-        finalEMA=currentEMA
 
-    print("Final EMA :"+ str(finalEMA))
+def calculateEMA(symbol, df, numdays):
+    lastRow = df.tail(1)
+    # print(lastRow)
+    # closingPriceToday = lastRow["Close"]
+    # print("closingPriceToday : "+str(closingPriceToday))
+    # print("Index"+ str(df.index[0]))
+    priordf = getPrice(symbol=symbol, today=df.index[0] - timedelta(days=1)
+                       , numdays=numdays)
+    # extractcloseData(priordf)
+    # print(priordf)
+    # print(priordf.count)
+    prevEMA = calculateMVA(priordf)
+    # print("First EMA => "+str(prevEMA))
+    finalEMA = 0
+    for ind in df.index:
+        currentEMA = emaforDate(prevEMA, df['Close'][ind], numdays)
+        print("Current EMA "+ str(currentEMA))
+        if math.isnan(currentEMA):
+            return None
+        else:
+        # print("EMA for "+str(ind)+"  "+str(currentEMA))
+        # cummulativeEMA=prevEMA+currentEMA
+            prevEMA = currentEMA
+            finalEMA = currentEMA
+    print("Final EMA :" + str(finalEMA))
     return finalEMA
 
 
-
-
-
 def emaforDate(emayesterDay, today, numdays):
-        multiplier = 2 / (numdays + 1)
-        emaToday=today*multiplier+emayesterDay*(1-multiplier)
-        return emaToday
+    multiplier = 2 / (numdays + 1)
+    print("Multiplier => "+str(multiplier))
+    emaToday = today * multiplier + emayesterDay * (1 - multiplier)
+    print("EMA Today => "+str(emaToday))
+    return emaToday
--- main.py
+++ main.py
@@ -8,32 +8,69 @@ from datetime import date, datetime
 from PriceHistoryExtraction import getPrice, extractcloseData, calculateMVA, calculateEMA
 import sys
 
-def mvaCaculator(symbol,today):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    date_object =datetime.strptime(today, '%Y-%m-%d').date()
-    finaldta= getPrice(symbol='SBIN',
-                   today=date_object ,numdays=20)
-                   #expdt=date(2022,12,12))
+from com.nsema.symbols.Symbols import Symbols
+from nsepy.symbols import get_symbol_list
+import pandas as pd
 
-    #print (finaldta)
-    #extractcloseData(finaldta)
-    sma=calculateMVA(finaldta)
-    print("Simple Moving average: " + str(sma))
-    extractcloseData(finaldta)
-    finaldta.to_csv(str(symbol)+'.csv')
-    ema=calculateEMA(symbol='SBIN',df=finaldta,numdays=20)
-    print("------------Final Results------------")
-    print("| SMA 20 for "+symbol+" is "+str(sma)+"  |")
-    print("| EMA 20 for " + symbol + " is "+str(ema)+" |")
-    print("|The data is available in "+str(symbol)+".csv |")
-    print("-------------------------------------")
+
+def mvaCaculator(symbol, today):
+    # Calculate Moving Average of a stock
+    symbolData = Symbols()
+    symboldf = get_symbol_list()
+    print(symboldf)
+    for ind in symboldf.index:
+        symbolforExtraction = symboldf['SYMBOL'][ind]
+        #symbolforExtraction = 'DTIL'
+        symbol = symbolforExtraction
+        if symbol == 'DVL':
+            print('Skip DVL stock')
+        else:
+            print(symbolforExtraction)
+            date_object = datetime.strptime(today, '%Y-%m-%d').date()
+            finaldta = getPrice(symbol=symbolforExtraction,
+                                today=date_object, numdays=20)
+            if finaldta.empty:
+                print('Skipping all further process for symbol ' + symbol + ' as the price data is empty')
+            else:
+                finaldf = finaldta.tail(1)
+                print(finaldf['Close'].item())
+                close = finaldf['Close'].item()
+                print("Value of close => " + str(close))
+                # print("Any of close => "+ any(close))
+                sma = calculateMVA(finaldta).astype('float')
+                print("Simple Moving average: " + str(sma))
+                extractcloseData(finaldta)
+                # finaldta.to_csv(str(symbol) + '.csv')
+                ema = calculateEMA(symbol=symbolforExtraction, df=finaldta, numdays=20)
+                if ema == None:
+                    print('Skipping all further process for symbol ' + symbol + ' as the price data is empty')
+                else:
+                    print("------------Final Results------------")
+                    print("| SMA 20 for " + symbol + " is " + str(sma) + "  |")
+                    print("| EMA 20 for " + symbol + " is " + str(ema) + " |")
+                    print("| Close Rate for " + symbol + " is " + str(close) + " |")
+                    print("|The data is available in " + str(symbol) + ".csv |")
+                    print("-------------------------------------")
+                    if close > ema:
+                        data = [{'Symbol': symbolforExtraction, 'Close': str(close), 'sma': sma, 'ema': ema}]
+                        df = pd.DataFrame(data, columns=['Symbol', 'Close', 'sma', 'ema'])
+                        print(df)
+                        symbolData.add_to_list(symbol=df)
+                    else:
+                        print("Close price is not greater than EMA for " + symbolforExtraction)
+                        print("------------Final Details below------------")
+                        print("| SMA 20 for " + symbol + " is " + str(sma) + "  |")
+                        print("| EMA 20 for " + symbol + " is " + str(ema) + " |")
+                        print("| Close Rate for " + symbol + " is " + str(close) + " |")
+                        print("|The data is available in " + str(symbol) + ".csv |")
+                        print("-------------------------------------")
+    symbolData.extract_data_to_csv()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     n = len(sys.argv)
     print("Total arguments passed:", n)
-    mvaCaculator(sys.argv[1],sys.argv[2])
+    mvaCaculator(sys.argv[1], sys.argv[2])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
